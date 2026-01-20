15809fee792f91c47624955dbaf9b17de2020b06
DavidB <david.bernard@fms-ea.com>
2026-01-20 11:46:36 +0100

    css fixed and button add to cart added to cards

--- components/molecular/ItemsDisplay.js
+++ components/molecular/ItemsDisplay.js
@@ -7,7 +7,7 @@ const ItemsDisplay = async (type, genre) => {
          @import url('https://fonts.googleapis.com/css2?family=Kosugi&display=swap');
 
         .items-display {
-        font-family: 'Kosugi', sans-serif;
+            font-family: 'Kosugi', sans-serif;
             display: grid;
             grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
             gap: 20px;
@@ -21,9 +21,15 @@ const ItemsDisplay = async (type, genre) => {
             padding: 10px;
             text-align: center;
             box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            gap: 10px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
         }
 
         .items-card img {
+            max-height: 150px;
             max-width: 100%;
             height: auto;
             border-radius: 5px;
@@ -53,6 +59,7 @@ const ItemsDisplay = async (type, genre) => {
             <img src="../../assets/${item.image}" alt="${item.title}" />
             <h3>${item.title}</h3>
             <p>${item.price}€</p>
+            <button style="cursor:pointer; border: none; box-shadow: 1px 1px 1px black;"><img src="../../assets/icon-shopping-cart.png" alt="Add to cart" style="width:20px; " /></button>
         </div>
     `).join('');   
 
