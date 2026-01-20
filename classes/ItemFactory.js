5f271b4bb7e9b0e7e1f525433caef226cf027e20
DavidB <david.bernard@fms-ea.com>
2026-01-20 13:22:09 +0100

    ItemFactory added

---
+++ classes/ItemFactory.js
@@ -0,0 +1,52 @@
+import Shoe from '../models/Shoe.js';
+import Clothing from '../models/Clothing.js';
+import Accessory from '../models/Accessory.js';
+
+
+class ItemFactory {
+
+    static createItem(data) {
+        switch (data.type) {
+            case 'shoe':
+                return new Shoe(
+                    data.id,
+                    data.title,
+                    data.type,
+                    data.genre,
+                    data.price,
+                    data.stock,
+                    data.image,
+                    data.size,
+                    data.brand
+                );
+            case 'clothing':
+                return new Clothing(
+                    data.id,
+                    data.title,
+                    data.type,
+                    data.genre,
+                    data.price,
+                    data.stock,
+                    data.image,
+                    data.size,
+                    data.material
+                );
+            case 'accessory':
+                return new Accessory(
+                    data.id,
+                    data.title,
+                    data.type,
+                    data.genre,
+                    data.price,
+                    data.stock,
+                    data.image,
+                    data.size,
+                    data.category
+                );
+            default:
+                throw new Error(`Unknown item type: ${data.type}`);
+        }  
+    }
+}
+
+export default ItemFactory;
