eced390366dfc2d6e3a77be292c4a3acfece0e4a
DavidB <david.bernard@fms-ea.com>
2026-01-20 09:43:20 +0100

    Sidebar css ok

--- components/molecular/SideBar.js
+++ components/molecular/SideBar.js
@@ -1,8 +1,32 @@
 
 
-const SideBar = ({ children }) => {
-  return (`
-    <aside className="sidebar">
+const SideBar = (children) => {
+
+    const style = `
+    <style>
+         @import url('https://fonts.googleapis.com/css2?family=Kosugi&display=swap');
+
+        .sidebar {
+        font-family: 'Kosugi', sans-serif;
+            width: 250px;
+            padding: 20px;
+            border-right: 1px solid #ccc;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            margin-top: 20px;
+            height: 100%;
+        }
+        .sidebar span {
+            font-weight: bold;
+            display: block;
+            margin-bottom: 15px;
+        }
+    </style>
+    `;
+
+  return ( style + `
+    <aside class="sidebar">
         <span>Filters</span>
         ${children}
     </aside>
