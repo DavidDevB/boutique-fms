e2b5b74490d13f65da322507a3c94b32cedb6d9e
DavidB <david.bernard@fms-ea.com>
2026-01-20 09:42:23 +0100

    userfilters css ok

---
+++ components/atomic/UserFilters.js
@@ -0,0 +1,60 @@
+
+
+const UserFilters = () => {
+
+    const style = `
+    <style>
+         @import url('https://fonts.googleapis.com/css2?family=Kosugi&display=swap');
+
+        .filters {
+        font-family: 'Kosugi', sans-serif;
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            gap: 10px;
+            margin-bottom: 20px;
+            width: 100%;
+            margin-top: 40px;
+            margin-left: 40px;
+        }
+        .filters span {
+            font-weight: bold;
+        }
+        .filters input[type="radio"] {
+            margin-left: 10px;
+        }
+        .filter-options  {
+            display: flex;
+            gap: 10px;
+            flex-direction: column;
+        }
+        .filter-option {
+            display: flex;
+            align-items: center;
+            gap: 5px;
+        }
+    </style>
+    `;
+
+    return (style +`
+        <div class="filters">
+            <span>Genre:</span>
+            <div class="filter-options">
+                <div class="filter-option">
+                    <input type="radio" id="men" name="genre" value="men"></input>
+                    <label for="men">Men</label>
+                </div>
+                <div class="filter-option">
+                    <input type="radio" id="women" name="genre" value="women"></input>
+                    <label for="women">Women</label>
+                </div>
+                <div class="filter-option">
+                    <input type="radio" id="all" name="genre" value="all"></input>
+                    <label for="all">All</label>
+                </div>
+            </div>
+        </div>
+    `);
+}
+
+export default UserFilters;
