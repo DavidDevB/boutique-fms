12199d4c00ea709a2de305523637c9b0918ff6ea
DavidB <david.bernard@fms-ea.com>
2026-01-20 09:44:13 +0100

    display of sidebar and userfilters in user.html ok

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,6 +1,11 @@
 import Header from '../../components/molecular/Header.js';
+import SideBar from '../../components/molecular/SideBar.js';
+import UserFilters from '../../components/atomic/UserFilters.js';
 
 const body = document.querySelector('body');
 
 // Insère le header en haut de la page
 body.insertAdjacentHTML('afterbegin', Header());
+
+// Insère la barre latérale à gauche de la page
+body.insertAdjacentHTML('beforeend', SideBar(UserFilters()));
