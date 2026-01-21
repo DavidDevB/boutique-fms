43f309eac60e858a39f3a32c94fce862d0576d0a
DavidB <david.bernard@fms-ea.com>
2026-01-21 09:42:08 +0100

    modal with articles in it added with fetch items.json

--- components/molecular/CartModal.js
+++ components/molecular/CartModal.js
@@ -1,6 +1,11 @@
 
 
-const CartModal = () => {
+const CartModal = async (storage) => {
+
+    const response = await fetch('../../data/items.json');
+    const data = await response.json();
+
+    if (storage === undefined) {
     return `
     <!-- Modal -->
     <div class="modal fade" id="cartModal" tabindex="-1" aria-labelledby="cartModalLabel" aria-hidden="true">
@@ -21,6 +26,34 @@ const CartModal = () => {
         </div>
     </div>
     `;
+    } else {
+        return `
+    <!-- Modal -->
+    <div class="modal fade" id="cartModal" tabindex="-1" aria-labelledby="cartModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="cartModalLabel">Your Shopping Cart</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <ul>
+                        ${Object.entries(storage).map(([itemId, quantity]) => {
+                            const item = data.items.find(i => i.id === parseInt(itemId));
+                        return `<div class="cart-item"><li>${item ? item.title : 'Unknown Item'}</li><div><button class="subtract" data-action="subtract"><img src="/assets/subtraction.png"/></button><input class="quantity-input" type="number" value="${quantity}"/><button class="add" data-action="add"><img src="/assets/addition.png"/></button></div></div>`;
+                        }).join('')}
+                    </ul>
+                </div>
+                <div class="modal-footer">
+                    <button id="close" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button id="checkout" type="button" class="btn btn-primary">Proceed to Checkout</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+    }
+
 }
 
 export default CartModal;
