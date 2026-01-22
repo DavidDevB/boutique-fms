a0fcad6825e71ab557c600e7964703fb0dad7cfa
DavidB <david.bernard@fms-ea.com>
2026-01-22 15:52:05 +0100

    modale can be opened on admin.html

--- pages/admin/admin.js
+++ pages/admin/admin.js
@@ -2,6 +2,7 @@ import Header from '../../components/molecular/Header.js';
 import SideBar from '../../components/molecular/SideBar.js';
 import AdminFilters from '../../components/atomic/AdminFilters.js';
 import OrdersDisplay from '../../components/molecular/OrdersDisplay.js';
+import EditJsonModal from '../../components/molecular/EditJsonModal.js';
 
 const cartStorage = localStorage;
 
@@ -10,6 +11,8 @@ const body = document.querySelector('body');
 // Insère le header en haut de la page
 body.insertAdjacentHTML('afterbegin', Header("admin"));
 
+// Insère le modal d'édition JSON
+body.insertAdjacentHTML('beforeend', await EditJsonModal());
 
 // Crée le conteneur principal pour la barre latérale et le contenu principal
 const mainContainer = document.createElement("div");
@@ -49,3 +52,46 @@ document.addEventListener('change', async (event) => {
     }
 });
 
+// Afficher la modale d'édition JSON
+document.addEventListener('click', (event) => {
+    if (event.target.matches('.add-item-button')) {
+        const editJsonModal = new bootstrap.Modal(document.getElementById('editJsonModal'));
+        editJsonModal.show();
+    }
+});
+
+// Ecouter les changements dans la modale d'édition JSON
+document.getElementById('save-item-json-button').addEventListener('click', async () => {
+    // Récupérer les valeurs des champs d'entrée
+    const response = await fetch('../../data/items.json')
+    const data = await response.json();
+
+    
+    // Générer un nouvel ID basé sur les IDs existants
+    const existingIds = data.items.map(item => item.id);
+    let itemId = 1;
+    while (existingIds.includes(itemId)) {
+        itemId++;
+    }
+
+    const newItem = {   
+        id: itemId,
+        name: document.getElementById('item-id-input').value,
+        type: document.getElementById('item-type-input').value,
+        genre: document.getElementById('item-genre-input').value,
+        color: document.getElementById('item-color-input').value,
+        size: document.getElementById('item-size-input').value,
+        price: parseFloat(document.getElementById('item-price-input').value),
+        stock: parseInt(document.getElementById('item-stock-input').value),
+        material: document.getElementById('item-material-input').value,
+        image: "default.png"
+    }
+
+    // Ajouter le nouvel article aux articles existants
+    data.items.push(newItem);
+    console.log("New Item JSON:", JSON.stringify(newItem, null, 2));
+
+    // Fermer la modale
+    const editJsonModal = bootstrap.Modal.getInstance(document.getElementById('editJsonModal'));
+    editJsonModal.hide();
+});
